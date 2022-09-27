509c53ff2d82277e7eff0d621efbee97d755d942
Langerz82 <langley.joshua@gmail.com>
2022-09-28 00:50:46 +1000

    crash fix

--- es-app/src/views/gamelist/ISimpleGameListView.cpp
+++ es-app/src/views/gamelist/ISimpleGameListView.cpp
@@ -386,11 +386,6 @@ void ISimpleGameListView::showSelectedGameSaveSnapshots()
 				return true;
 			}));
 		};
-		while (wait)
-		{
-			std::this_thread::yield();
-			std::this_thread::sleep_for(std::chrono::milliseconds(100));				
-		}
 		loadCloudWait();		
 #else
 		mWindow->pushGui(new GuiSaveState(mWindow, cursor, [this, cursor](SaveState state)

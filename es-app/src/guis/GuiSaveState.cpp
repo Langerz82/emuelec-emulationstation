7e82ddeb58adf86677ef30fb93ad09896122b430
Langerz82 <langley.joshua@gmail.com>
2022-09-28 16:53:38 +1000

    fix i think

--- es-app/src/guis/GuiSaveState.cpp
+++ es-app/src/guis/GuiSaveState.cpp
@@ -127,7 +127,7 @@ GuiSaveState::GuiSaveState(Window* window, FileData* game, const std::function<v
 			});
 			loading->setPosition(0,0);
 			loading->setSize(Renderer::getScreenWidth(), Renderer::getScreenHeight());
-			
+			window->pushGui(loading);
 		};
 		loadCloudWait();
 	} else {

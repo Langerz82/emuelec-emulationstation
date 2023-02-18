a6885c2d49c8ae9cc3a8e925e1e852680c3ab907
Langerz82 <langley.joshua@gmail.com>
2023-02-18 22:53:41 +1100

    t34

--- es-app/src/guis/GuiGameOptions.cpp
+++ es-app/src/guis/GuiGameOptions.cpp
@@ -534,8 +534,12 @@ void GuiGameOptions::hideGame(FileData* file)
 	auto sourceFile = file->getSourceFileData();
 	
 	file->setMetadata(MetaDataId::Hidden, "true");
-	file->getMetadata().setDirty();
-	ViewController::get()->onFileChanged(file, FILE_METADATA_CHANGED);
+	//file->getMetadata().setDirty();	
+	//ViewController::get()->onFileChanged(file, FILE_METADATA_CHANGED);
+	
+	sourceFile->setMetadata(MetaDataId::Hidden, "true");
+	sourceFile->getMetadata().setDirty();
+	ViewController::get()->onFileChanged(sourceFile, FILE_METADATA_CHANGED);
 
 	auto sys = sourceFile->getSystem();
 	if (sys->isGroupChildSystem())
@@ -549,8 +553,8 @@ void GuiGameOptions::hideGame(FileData* file)
 	}
 	else
 	{
-		//sys->getRootFolder()->removeFromVirtualFolders(sourceFile);
-		//delete sourceFile;
+		sys->getRootFolder()->removeFromVirtualFolders(sourceFile);
+		delete sourceFile;
 	}
 }
 

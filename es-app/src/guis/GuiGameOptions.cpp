24bfc8a48c1d45547652a39881a7877c20477067
Langerz82 <langley.joshua@gmail.com>
2023-02-18 21:40:50 +1100

    t24

--- es-app/src/guis/GuiGameOptions.cpp
+++ es-app/src/guis/GuiGameOptions.cpp
@@ -544,7 +544,7 @@ void GuiGameOptions::hideGame(FileData* file)
 	CollectionSystemManager::get()->deleteCollectionFiles(sourceFile);
 
 	auto view = ViewController::get()->getGameListView(sys, false);
-	auto sview = dynamic_cast<ISimpleGameListView*>(view.get());
+	ISimpleGameListView* sview = dynamic_cast<ISimpleGameListView*>(view.get());
 	if (view != nullptr) {
 		if (sview != nullptr) {
 			int index = sview->getCursorIndex();

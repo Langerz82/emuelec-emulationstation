3accf6fdab9ef061503747ccb3364868e3e73c5d
Langerz82 <langley.joshua@gmail.com>
2023-03-04 11:50:07 +1100

    f11

--- es-app/src/guis/GuiGameOptions.cpp
+++ es-app/src/guis/GuiGameOptions.cpp
@@ -552,25 +552,25 @@ void GuiGameOptions::moveToJunkGame(FileData* file)
 		LOG(LogInfo) << "strMkDir:" << strMkDir.c_str();
 	}
 
-	/*if (!Utils::FileSystem::exists(strMkDir.c_str())) {
+	if (!Utils::FileSystem::exists(strMkDir.c_str())) {
 		Utils::FileSystem::createDirectory(strMkDir.c_str());
 		FileData* newFolder = new FileData(FOLDER, "junk", sourceFile->getSystem());
-		sourceFile->getSystem()->getRootFolder()->addChild(newFolder);
+		/*sourceFile->getSystem()->getRootFolder()->addChild(newFolder);
 		if (view != nullptr) {
 				view.get()->repopulate();
 				view->setCursor(newFolder);
-		}
+		}*/
 		//ViewController::get()->reloadGameListView(sys);
-	}*/
+	}
 
 	char cmdMvFile[255];
   snprintf(cmdMvFile, sizeof(cmdMvFile), "mv \"%s\" ./junk", sourceFile->getFullPath().c_str());
   std::string strMvFile = cmdMvFile;
 
 	LOG(LogInfo) << "strMvFile:" << strMvFile.c_str();
-	//system(strMvFile.c_str());
+	system(strMvFile.c_str());
 	
-	delete sourceFile;
+	//delete sourceFile;
 }
 
 #endif

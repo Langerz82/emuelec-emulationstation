98debb0da132b462ebc220941c5f930aa42661c9
Joshua L <Langerz82@users.noreply.github.com>
2023-05-10 07:54:46 +1000

    save defs regardless.

--- es-app/src/guis/GuiMenu.cpp
+++ es-app/src/guis/GuiMenu.cpp
@@ -4972,18 +4972,13 @@ void GuiMenu::popSpecificConfigurationGui(Window* mWindow, std::string title, st
 	{
 		[&] {
 			std::string prefixName = tEmulator;
-			if (SystemConf::getInstance()->get(prefixName + ".joy_btns").empty() ||
-					SystemConf::getInstance()->get(prefixName + ".joy_btn_names").empty() ||
-					SystemConf::getInstance()->get(prefixName + ".joy_btn_indexes").empty())
-			{
-				SystemConf::getInstance()->set(prefixName + ".joy_btns", "input a button,input b button,input x button,input y button,input l button,input r button,input l2 button,input r2 button");
-				SystemConf::getInstance()->set(prefixName + ".joy_btn_indexes", "1,2" );
-        SystemConf::getInstance()->set(prefixName + ".joy_btn_names", "mk,sf" );
-        SystemConf::getInstance()->set(prefixName + ".joy_btn_order0", "0 1 2 3 4 5 6 7" );
-        SystemConf::getInstance()->set(prefixName + ".joy_btn_order1", "2 1 3 0 4 5 6 7" );
-        SystemConf::getInstance()->set(prefixName + ".joy_btn_order2", "0 1 3 4 2 5 6 7" );
-				SystemConf::getInstance()->saveSystemConf();
-			}
+			SystemConf::getInstance()->set(prefixName + ".joy_btns", "input a button,input b button,input x button,input y button,input l button,input r button,input l2 button,input r2 button");
+			SystemConf::getInstance()->set(prefixName + ".joy_btn_indexes", "1,2" );
+      SystemConf::getInstance()->set(prefixName + ".joy_btn_names", "mk,sf" );
+      SystemConf::getInstance()->set(prefixName + ".joy_btn_order0", "0 1 2 3 4 5 6 7" );
+      SystemConf::getInstance()->set(prefixName + ".joy_btn_order1", "2 1 3 0 4 5 6 7" );
+      SystemConf::getInstance()->set(prefixName + ".joy_btn_order2", "0 1 3 4 2 5 6 7" );
+			SystemConf::getInstance()->saveSystemConf();
 
 			int btnCfgIndex = atoi(SystemConf::getInstance()->get(configName + ".joy_btn_cfg").c_str());
 			std::vector<int> remapIndexes = int_explode( SystemConf::getInstance()->get(prefixName + ".joy_btn_indexes"));
